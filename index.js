2932a22ff61f6eb0292d9b9e13a1f7100e120d3d
David Shim <dsfm26@hotmail.com>
2021-06-21 22:39:23 +0900

    Add event listener for key press

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 var buttons = document.querySelectorAll(".drum");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      default:
-        console.log(buttonInnerHTML);
-    }
+    checkAndPlayInstrument(this.innerHTML);
   });
+}
+
+document.addEventListener("keydown", function (event) {
+  checkAndPlayInstrument(event.key);
+});
+
+function checkAndPlayInstrument(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+  }
 }
